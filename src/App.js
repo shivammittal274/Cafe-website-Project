95f6ff85b6343e14eadc69f48721f7a7e8f92c37
shivammittal274 <mittal.shivam103@gmail.com>
2021-01-14 17:24:32 +0530

    First update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="firstPage"><Header /></div>
+    </div>
+  );
+}
+
+export default App;
